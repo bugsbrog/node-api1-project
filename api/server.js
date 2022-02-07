8b4e52c95fa8c2124ce96e3b5db33fa7e8e664a8
bugsbrog <bugsbrog@gmail.com>
2022-02-07 12:56:46 -0700

    post user endpoint done and working

--- api/server.js
+++ api/server.js
@@ -33,4 +33,28 @@ server.get('/api/users/:id', async (req, res) => {
             }
 })
 
+server.post('/api/users', async (req, res) => {
+        try {
+            const { name, bio } = req.body
+            const postUser = await Users.insert({ name, bio })
+                if (!name || !bio) {
+                    res.status(400).json({
+                        message: 'Please provide name and bio for the user'
+                    })
+                } else {
+                    res.status(201).json(postUser)
+                }
+            } catch {
+                res.status(500).json({
+                    message: 'There was an error while saving the user to the database'
+                })
+            }
+})
+
+
+
+
+
+
+
 module.exports = server
